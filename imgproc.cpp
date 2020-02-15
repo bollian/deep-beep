d04a25fe61b8666f064121ca2aafb2d6f3bff723
hmatt12 <hmatt12@vt.edu>
2020-02-15 13:22:00 -0500

    pushing the current line detection

--- imgproc.cpp
+++ imgproc.cpp
@@ -12,19 +12,32 @@ int main(int argc, char **argv) {
 	//proccess pipeline
     cv::Mat image;
     image = cv::imread(argv[1]);
-  
-    cv::Mat binary;
-    cv::inRange(image, cv::Scalar(0, 0, 0), cv::Scalar(128, 128, 128), binary); 
+  	
+	cv::GaussianBlur( image, image, cv::Size( 13, 0 ), 0, 0.01 );	
+
+	cv::pyrDown(image, image, cv::Size(image.cols /2, image.rows / 2));
 	
-    std::vector<cv::Vec2f> houghOut;
-    cv::HoughLines(binary, houghOut, 1, CV_PI /180, 150, 0, CV_PI/180); 
-	//display steps
+
+    cv::Mat binary;
+    cv::inRange(image, cv::Scalar(0, 0, 0), cv::Scalar(185, 185, 185), binary); 
+	
+    std::vector<cv::Vec4i> houghOut;
+    cv::HoughLinesP(binary, houghOut, 1, CV_PI /180, 150, 150 ,40); 
+
+	for(int j = 0; j < houghOut.size(); j++)
+        {
+		std::cout << houghOut[j][0] << "\t" << houghOut[j][1] << "\t" << houghOut[j][2] << "\t" <<  houghOut[j][3] << std::endl;
+                cv::line( image, cv::Point(houghOut[j][0], houghOut[j][1]), cv::Point(houghOut[j][2], houghOut[j][3]), cv::Scalar(255,0,0), 3, cv::LINE_AA);
+        }
+    
+    //display steps
    cv::namedWindow("Step 0", cv::WINDOW_AUTOSIZE);
    cv::imshow("Step 0", image);
    
    cv::namedWindow("Step 1", cv::WINDOW_AUTOSIZE);
    cv::imshow("Step 1", binary);
-   std::vector<std::vector<cv::Vec2f>> staves;
+
+  /* std::vector<std::vector<cv::Vec2f>> staves;
    std::vector<cv::Vec2f> potentialStaff;
    double staffAngle;
    int staffCount = 5;
@@ -32,38 +45,56 @@ int main(int argc, char **argv) {
    for(auto i = houghOut.begin(); i != houghOut.end(); i++)
    {
 	double angle = (*i)[1];
-	if(!(angle > 1.56 && angle < 1.58))
+	if(!(angle > 1.5 && angle < 1.7))
 	{
 		houghOut.erase(i);
 		i--;
 	}
    }
 
+ for(int j = 0; j < houghOut.size(); j++)
+        {
+                std::cout << houghOut[j][0] << "\t" << houghOut[j][1] << std::endl;
+
+                float rho = houghOut[j][0], theta = houghOut[j][1];
+                cv::Point pt1, pt2;
+                double a = cos(theta), b = sin(theta);
+                double x0 = a*rho, y0 = b*rho;
+                pt1.x = cvRound(x0 + 1000*(-b));
+                pt1.y = cvRound(y0 + 1000*(a));
+                pt2.x = cvRound(x0 - 1000*(-b));
+                pt2.y = cvRound(y0 - 1000*(a));
+                cv::line( image, pt1, pt2, cv::Scalar(0,255,0), 3, cv::LINE_AA);
+        }
+
+
    for(int i = 0; i < houghOut.size() - 2; i++)
    {
 	potentialStaff.push_back(houghOut[i]);
 	for(int j = i + 1; j < houghOut.size() - 1; j++)
 	{
-		potentialStaff.push_back(houghOut[j]);
-		int staffStep = potentialStaff[1][0] - potentialStaff[0][0];
-		for(int k = j + 1; k < houghOut.size(); k++)
+		if( abs(houghOut[j][1] - potentialStaff[0][1]) < .05)
 		{
-			if(abs(houghOut[k][0] - potentialStaff[potentialStaff.size() - 1][0] - staffStep) < 1)
-					{
-						potentialStaff.push_back(houghOut[k]);
-					}
-		}
+			potentialStaff.push_back(houghOut[j]);
+			int staffStep = potentialStaff[1][0] - potentialStaff[0][0];
+			for(int k = j + 1; k < houghOut.size(); k++)
+			{
+				if(abs(houghOut[k][0] - potentialStaff[potentialStaff.size() - 1][0] - staffStep) < 1 && abs(houghOut[k][1] - potentialStaff[0][1]) < .05)
+				{
+					potentialStaff.push_back(houghOut[k]);
+				}
+			}
 
-		if(potentialStaff.size() == 5)
-		{
-			staves.push_back(std::vector<cv::Vec2f>(potentialStaff));
-			break;
+			if(potentialStaff.size() == 5)
+			{
+				staves.push_back(std::vector<cv::Vec2f>(potentialStaff));
+				break;
+			}
+			while(potentialStaff.size() > 1)
+			{
+				potentialStaff.pop_back();
+			}
 		}
-		while(potentialStaff.size() > 1)
-		{
-			potentialStaff.pop_back();
-		}
-		
 	}
 	potentialStaff.clear();
    }
@@ -77,10 +108,10 @@ int main(int argc, char **argv) {
 		cv::Point pt1, pt2;
         	double a = cos(theta), b = sin(theta);
         	double x0 = a*rho, y0 = b*rho;
-		pt1.x = cvRound(x0 + 1000*(-b));
-		pt1.y = cvRound(y0 + 1000*(a));
-		pt2.x = cvRound(x0 - 1000*(-b));
-		pt2.y = cvRound(y0 - 1000*(a));
+		pt1.x = cvRound(x0 + 2000*(-b));
+		pt1.y = cvRound(y0 + 2000*(a));
+		pt2.x = cvRound(x0 - 2000*(-b));
+		pt2.y = cvRound(y0 - 2000*(a));
 		cv::line( image, pt1, pt2, cv::Scalar(0,0,255), 3, cv::LINE_AA);
 	}
    }
@@ -89,7 +120,7 @@ int main(int argc, char **argv) {
    cv::namedWindow("Step 3*", cv::WINDOW_AUTOSIZE);
    cv::imshow("Step 3*", image);
 
-
+*/
    
    cv::waitKey();
 
